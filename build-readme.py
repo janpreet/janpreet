ac142638915722af877b0f3d140b82b18ac6353c
Janpreet Singh <janpreetsinghgill@gmail.com>
2024-08-27 08:09:46 -0400

    improvements due for 4 years

--- build-readme.py
+++ build-readme.py
@@ -3,72 +3,98 @@ import requests
 import feedparser
 import os
 from collections import Counter
-import write2list
+from datetime import datetime, timezone
+import matplotlib.pyplot as plt
+import io
+import base64
 
-# Language Composition
+def get_github_data(username, token=None):
+    headers = {'Authorization': f'token {token}'} if token else {}
+    all_repos = []
+    page = 1
+    while True:
+        response = requests.get(f"https://api.github.com/users/{username}/repos?per_page=100&page={page}", headers=headers)
+        repos = json.loads(response.text)
+        if not repos:
+            break
+        all_repos.extend(repos)
+        page += 1
+    return all_repos
 
-print('Collecting Language Data...')
-username='janpreet'
-response = requests.get("https://api.github.com/users/"+username+"/repos")
-userData = json.loads(response.text)
+def get_language_data(username, repos, token=None):
+    headers = {'Authorization': f'token {token}'} if token else {}
+    language_data = Counter()
+    for repo in repos:
+        response = requests.get(repo['languages_url'], headers=headers)
+        repo_languages = json.loads(response.text)
+        language_data.update(repo_languages)
+    return language_data
 
-allLanguages=[]
+def get_language_composition(language_data):
+    total = sum(language_data.values())
+    return {lang: count / total * 100 for lang, count in language_data.items()}
 
-for obj in userData:
-    if 'language' in obj:
-        if obj["language"] != None:
-            allLanguages.extend([obj["language"]])
+def create_language_chart(lang_composition):
+    plt.figure(figsize=(10, 5))
+    langs = list(lang_composition.keys())
+    sizes = list(lang_composition.values())
+    plt.pie(sizes, labels=langs, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.title("Language Composition")
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    return base64.b64encode(buffer.getvalue()).decode()
 
-langCount=Counter(allLanguages)
+def get_blog_posts(blog_url, max_posts=5):
+    feed = feedparser.parse(blog_url)
+    return [{'title': e.get('title', ''), 'link': e.get('link', '')} 
+            for e in feed['entries'][:max_posts]]
 
-langComposition=[]
-for key, size in sorted(langCount.items(), key=lambda x: x[1], reverse=True):
-    langComposition.append('{}: {:0.2f}'.format('- '+key, ((size/len(allLanguages))*100))+'% <br />')
+def generate_readme_content(template_path, **kwargs):
+    with open(template_path, 'r') as f:
+        template = f.read()
+    
+    blog_posts_formatted = '\n'.join([f"- [{post['title']}]({post['link']})" for post in kwargs['blog_posts']])
+    kwargs['blog_posts'] = blog_posts_formatted
 
-# Blog Feed
+    return template.format(**kwargs)
 
-print('Collecting Blog Feed...')
-blogUrl='https://janpreet.com/feed.xml'
+def main():
+    username = 'janpreet'
+    blog_url = 'https://janpreet.com/feed.xml'
+    github_token = os.environ.get('GITHUB_TOKEN')
+    template_path = 'readme_template.md'
 
-f = feedparser.parse(blogUrl)
+    print('Collecting GitHub data...')
+    repos = get_github_data(username, github_token)
+    
+    print('Analyzing language composition...')
+    language_data = get_language_data(username, repos, github_token)
+    lang_composition = get_language_composition(language_data)
+    
+    print('Creating language chart...')
+    chart_image = create_language_chart(lang_composition)
+    
+    print('Fetching blog posts...')
+    blog_posts = get_blog_posts(blog_url)
+    
+    print('Generating README content...')
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
+    readme_content = generate_readme_content(
+        template_path,
+        username=username,
+        chart_image=chart_image,
+        blog_posts=blog_posts,
+        current_time=current_time
+    )
+    
+    print('Writing README.md...')
+    with open('README.md', 'w', encoding='utf-8') as f:
+        f.write(readme_content)
+    
+    print('Finished!')
 
-postList=[]
-for e in f['entries']:
-    postList.append('{}{}{}'.format('- <a href="'+e.get('link', '')+'" target="_blank">', e.get('title', ''),'</a><br />'))    
-
-# Readme Content
-
-print('Processing Readme Data...')
-
-fixedContent=["![Build Profile](https://github.com/janpreet/janpreet/workflows/Build%20README/badge.svg) <br />" \
-            "<h3>Hi there 👋 </h3> <br />" \
-            "Thank you for visiting my GitHub. You may reach out to me at [singh@janpreet.com](mailto:singh@janpreet.com), " \
-            "or read my [blog](https://janpreet.com). <br />" \
-            "<table style='float:right' markdown='1'><tr><th>Language Composition</th><th>Blog</th></tr><tr><td style='vertical-align:top' markdown='1'> "]
-
-separator=["</td><td style='vertical-align:top' markdown='1'>"]
-
-endNote=["</td></tr></table>" \
-            "<small><i>NOTE: Language composition is a list of most used languages in my repositories." \
-            "It is not a direct indication of my skill level.</i></small>"]
-
-readmeTuple=(fixedContent, langComposition, separator, postList, endNote)
-
-readmeContent=write2list.create(readmeTuple)
-
-# Create Readme.md
-
-print('Writing Readme.md...')
-
-readmeFile='./README.md'
-
-if not os.path.exists(readmeFile):
-    open(readmeFile, 'w').close()
-
-with open(readmeFile, mode='wt', encoding='utf-8') as f:
-    try:
-        f.write('\n'.join(str(v) for v in readmeContent))  
-    finally:
-        f.close()    
-
-print('Finished!')        
+if __name__ == "__main__":
+    main()
---
+++ readme_template.md
@@ -0,0 +1,17 @@
+# Welcome to my GitHub Profile! 👋
+
+## About Me
+I'm passionate about coding and always learning new technologies. Feel free to explore my repositories and don't hesitate to reach out if you have any questions or collaboration ideas!
+
+## Language Composition
+<img src="data:image/png;base64,{chart_image}" alt="Language Composition" width="500"/>
+
+## Recent Blog Posts
+{blog_posts}
+
+## Get in Touch
+- Email: [kado@janpreet.com](mailto:kado@janpreet.com)
+- Blog: [https://janpreet.com](https://janpreet.com)
+
+<small><i>Last updated: {current_time}</i></small>
+<small><i>NOTE: Language composition is a list of most used languages in my repositories. It is not a direct indication of my skill level.</i></small>
